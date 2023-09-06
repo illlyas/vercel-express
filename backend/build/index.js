772805f83a344f051c2117bd5ce218afe5d4974c
fanmd <fan.robin@fanruan.com>
2023-09-06 09:57:23 +0800

    test

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,24 +1,55 @@
-const app = require('express')();
-const helmet = require('helmet');
-const compression = require('compression');
+/* eslint-disable */
+import express from 'express';
+const server = express();
+import path from'path';
+import history  from 'connect-history-api-fallback';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const distPath = path.resolve(__dirname, 'dist');
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-app.use(helmet());
-app.use(compression());
+server.use('/app/mobile', express.static(distPath));
+server.use(history());
 
-app.get('/api', (req, res) => {
-  const randomId = `${Math.random()}`.slice(2);
-  const path = `/api/item/${randomId}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+const port =  process.env.PORT || 4000;
+
+
+// 代理开发使用
+const proxyTarget = 'https://dev.jiandaoyun.com';
+// 本地开发使用
+// const proxyTarget = 'http://localhost:3000';
+
+server.use(
+  '/',
+  createProxyMiddleware({
+    target: proxyTarget,
+    changeOrigin: true,
+    cookieDomainRewrite: '127.0.0.1:' + port,
+    ws: true,
+    prependPath: false,
+    headers: {
+      Accept: 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      host: 'http://127.0.0.1:' + port
+    },
+    router: {
+      '/socket': 'https://messaging-dev.jiandaoyun.com'
+    },
+    onProxyRes(proxyRes, req, res) {
+      const key = 'set-cookie';
+      if (proxyRes.headers[key]) {
+        proxyRes.headers[key] = proxyRes.headers[key].map((cookie) => {
+          const splitCookie = cookie.split(' ');
+          return [splitCookie[0], 'Path=/'].join(' ');
+        });
+      }
+    }
+  })
+);
+
+server.listen(port, () => {
+  console.log('we are now listen at:http://127.0.0.1:' + port);
 });
 
-app.get('/api/item/:itemId', (req, res) => {
-  const { itemId } = req.params;
-  res.json({ itemId });
-});
-
-const port = process.env.PORT || 3030;
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
-
-module.exports = app;
+module.export = server;
